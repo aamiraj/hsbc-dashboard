39f0c8f8cac298a53ee4e1a1f44495cd0423a8e2
farukphero <farukk6319@gmail.com>
2024-03-10 01:24:57 +0600

    commitment done

--- src/app/(customer)/accounts/commitment/page.jsx
+++ src/app/(customer)/accounts/commitment/page.jsx
@@ -1,11 +1,153 @@
-import React from 'react';
+import React from "react";
 
 const SituationPage = () => {
-    return (
-        <div>
-            SituationPage
+  return (
+    <div className="mt-7 mb-16 rounded-xl bg-white w-[852px] h-[610px]">
+      <h1 className="text-[16px] text-[#595959] font-bold px-10 pt-6">
+        History
+      </h1>
+      <ul className="flex justify-between px-8 pt-12 pb-4 border-b border-[#878790]  mx-11">
+        <li className="text-sm text-[#595959] font-medium">Investation</li>
+        <li className="text-sm text-[#595959] font-medium invisible">
+          Variationgjgj
+        </li>
+        <li className="text-sm text-[#595959] font-medium">Variation</li>
+        <li className="text-sm text-[#595959] font-medium">Profit/Loss</li>
+        <li className="text-sm text-[#595959] font-medium">Num Postitions</li>
+        <li className="text-sm text-[#595959] font-medium">Comintment</li>
+      </ul>
+      <ul className="flex justify-around border-b border-[#878790] mx-11">
+        <li className="font-medium flex items-center text-[13px] text-[#DD9A19] w-12 whitespace-nowrap">Stock Market</li>
+        <div className="border-l border-r border-[#878790] py-4 w-[490px]">
+        <div className="flex justify-between   border-b border-[#878790] px-8 pb-4">
+          <li className=" font-normal text-center text-[13px] text-[#0052B4]">
+            Open
+          </li>
+          <li className="text-[13px] font-normal text-[#0052B4]">
+            0,0%
+          </li>
+          <li className="text-[13px] font-normal text-[#0052B4]">$0</li>
+          <li className="text-[13px] font-normal text-[#0052B4]">
+            0 Positions
+          </li>
         </div>
-    );
+        <div className="flex justify-between px-8 pt-4 w-[490px]">
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          Finised
+          </li>
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          0,13%
+          </li>
+          <li className="text-[13px] font-normal text-[#0F993E]">$19.12</li>
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          14 Positions
+          </li>
+        </div>
+        </div>
+        <li className="text-[13px] font-bold text-[#0F993E] flex items-center w-12">
+          $190{" "}
+        </li>
+      </ul>
+      <ul className="flex justify-around border-b border-[#878790] mx-11">
+        <li className="font-medium flex items-center text-[13px] text-[#DD9A19] w-12 whitespace-nowrap">Car Park</li>
+        <div className="border-l border-r border-[#878790] py-4 w-[490px]">
+        <div className="flex justify-between   border-b border-[#878790] px-8 pb-4">
+          <li className=" font-normal text-center text-[13px] text-[#0052B4]">
+            Open
+          </li>
+          <li className="text-[13px] font-normal text-[#0052B4]">
+            0,0%
+          </li>
+          <li className="text-[13px] font-normal text-[#0052B4]">$0</li>
+          <li className="text-[13px] font-normal text-[#0052B4]">
+            0 Positions
+          </li>
+        </div>
+        <div className="flex justify-between px-8 pt-4 w-[490px]">
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          Finised
+          </li>
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          0,13%
+          </li>
+          <li className="text-[13px] font-normal text-[#0F993E]">$19.12</li>
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          14 Positions
+          </li>
+        </div>
+        </div>
+        <li className="text-[13px] font-bold text-[#0F993E] flex items-center w-12">
+          $190{" "}
+        </li>
+      </ul>
+      <ul className="flex justify-around border-b border-[#878790] mx-11">
+        <li className="font-medium flex items-center text-[13px] text-[#DD9A19] w-12 whitespace-nowrap">Saving Fund</li>
+        <div className="border-l border-r border-[#878790] py-4 w-[490px]">
+        <div className="flex justify-between   border-b border-[#878790] px-8 pb-4">
+          <li className=" font-normal text-center text-[13px] text-[#0052B4]">
+            Open
+          </li>
+          <li className="text-[13px] font-normal text-[#0052B4]">
+            0,0%
+          </li>
+          <li className="text-[13px] font-normal text-[#0052B4]">$0</li>
+          <li className="text-[13px] font-normal text-[#0052B4]">
+            0 Positions
+          </li>
+        </div>
+        <div className="flex justify-between px-8 pt-4 w-[490px]">
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          Finised
+          </li>
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          0,13%
+          </li>
+          <li className="text-[13px] font-normal text-[#0F993E]">$19.12</li>
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          14 Positions
+          </li>
+        </div>
+        </div>
+        <li className="text-[13px] font-bold text-[#0F993E] flex items-center w-12">
+          $190{" "}
+        </li>
+      </ul>
+      <ul className="flex justify-around border-b border-[#878790] mx-11">
+        <li className="font-medium flex items-center text-[13px] text-[#DD9A19] w-12 whitespace-nowrap">Stock Market</li>
+        <div className="border-l border-r border-[#878790] py-4 w-[490px]">
+        <div className="flex justify-between   border-b border-[#878790] px-8 pb-4">
+          <li className=" font-normal text-center text-[13px] text-[#0052B4]">
+            Open
+          </li>
+          <li className="text-[13px] font-normal text-[#0052B4]">
+            0,0%
+          </li>
+          <li className="text-[13px] font-normal text-[#0052B4]">$0</li>
+          <li className="text-[13px] font-normal text-[#0052B4]">
+            0 Positions
+          </li>
+        </div>
+        <div className="flex justify-between px-8 pt-4 w-[490px]">
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          Finised
+          </li>
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          0,13%
+          </li>
+          <li className="text-[13px] font-normal text-[#0F993E]">$19.12</li>
+          <li className="text-[13px] font-normal text-[#0F993E]">
+          14 Positions
+          </li>
+        </div>
+        </div>
+        <li className="text-[13px] font-bold text-[#0F993E] flex items-center w-12">
+          $190{" "}
+        </li>
+      </ul>
+       
+      
+    </div>
+  );
 };
 
 export default SituationPage;
